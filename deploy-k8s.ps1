db71207f81d0856f3d5e156eed8903588e1004fe
xGremlinx <vdeleu6@gmail.com>
2026-05-27 14:09:14 +0300

    Fix

--- deploy-k8s.ps1
+++ deploy-k8s.ps1
@@ -1,3 +1,6 @@
+docker pull 082319703213.dkr.ecr.us-east-1.amazonaws.com/backend-app:latest
+
+docker pull 082319703213.dkr.ecr.us-east-1.amazonaws.com/frontend-app:latest
 
 kubectl rollout restart deployment backend-deployment
 
