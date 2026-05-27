53caaf9d2a7e9918005a6a019b29e5131aa36f20
xGremlinx <vdeleu6@gmail.com>
2026-05-27 12:49:38 +0300

    Add automatic EKS deployment

--- deploy-k8s.ps1
+++ deploy-k8s.ps1
@@ -1,6 +1,3 @@
-docker pull 082319703213.dkr.ecr.us-east-1.amazonaws.com/backend-app:latest
-
-docker pull 082319703213.dkr.ecr.us-east-1.amazonaws.com/frontend-app:latest
 
 kubectl rollout restart deployment backend-deployment
 
